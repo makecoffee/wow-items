a7039a519830a4ad1e9f5cd2ece0af096b710e4b
makecoffee <256205470+makecoffee@users.noreply.github.com>
2026-03-17 13:57:59 +0300

    refactor item listener

--- src/main/java/net/makecoffee/items/listener/ItemListener.java
+++ src/main/java/net/makecoffee/items/listener/ItemListener.java
@@ -6,40 +6,36 @@ import net.minestom.server.event.Event;
 import net.minestom.server.event.EventNode;
 import net.minestom.server.event.item.PlayerBeginItemUseEvent;
 import net.minestom.server.event.item.PlayerFinishItemUseEvent;
+import net.minestom.server.item.ItemStack;
 import net.minestom.server.item.component.UseCooldown;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
 
 public final class ItemListener {
 
     public ItemListener(@NotNull EventNode<@NotNull Event> node, @NotNull CooldownManager cooldownManager) {
         node.addListener(PlayerBeginItemUseEvent.class, event -> {
-            UseCooldown cooldown = event.getItemStack().get(DataComponents.USE_COOLDOWN);
-            if (cooldown == null) {
-                return;
-            }
-
-            String cooldownGroup = cooldown.cooldownGroup();
-            if (cooldownGroup == null) {
-                return;
-            }
-
-            if (cooldownManager.has(event.getPlayer(), cooldownGroup)) {
-                event.setCancelled(true);
-            }
+            Optional<UseCooldown> optional = cooldown(event.getItemStack());
+            optional.ifPresent(cooldown -> {
+                assert cooldown.cooldownGroup() != null;
+                if (cooldownManager.has(event.getPlayer(), cooldown.cooldownGroup())) {
+                    event.setCancelled(true);
+                }
+            });
         });
 
         node.addListener(PlayerFinishItemUseEvent.class, event -> {
-            UseCooldown cooldown = event.getItemStack().get(DataComponents.USE_COOLDOWN);
-            if (cooldown == null) {
-                return;
-            }
-
-            String cooldownGroup = cooldown.cooldownGroup();
-            if (cooldownGroup == null) {
-                return;
-            }
-
-            cooldownManager.add(event.getPlayer(), cooldownGroup, cooldown.seconds());
+            Optional<UseCooldown> optional = cooldown(event.getItemStack());
+            optional.ifPresent(cooldown -> {
+                assert cooldown.cooldownGroup() != null;
+                cooldownManager.add(event.getPlayer(), cooldown.cooldownGroup(), cooldown.seconds());
+            });
         });
+    }
+
+    private @NotNull Optional<UseCooldown> cooldown(@NotNull ItemStack is) {
+        return Optional.ofNullable(is.get(DataComponents.USE_COOLDOWN))
+                .filter(cooldown -> cooldown.cooldownGroup() != null);
     }
 }
