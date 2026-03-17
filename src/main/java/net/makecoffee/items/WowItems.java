c696902e8738eb2573897b59ae9c3b6fe7a871c2
makecoffee <256205470+makecoffee@users.noreply.github.com>
2026-03-17 13:09:24 +0300

    fix description nullability

--- src/main/java/net/makecoffee/items/WowItems.java
+++ src/main/java/net/makecoffee/items/WowItems.java
@@ -75,5 +75,4 @@ public final class WowItems {
     public ItemService itemService() {
         return itemService;
     }
-
 }
--- src/main/java/net/makecoffee/items/data/ItemDisplayData.java
+++ src/main/java/net/makecoffee/items/data/ItemDisplayData.java
@@ -5,4 +5,10 @@ import net.kyori.adventure.text.Component;
 import java.util.List;
 
 public record ItemDisplayData(Component name, List<Component> description, List<DataComponentEntry> components) {
+
+    public ItemDisplayData {
+        if (description == null) {
+            description = List.of();
+        }
+    }
 }
